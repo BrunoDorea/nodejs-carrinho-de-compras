482056f459bad6c25424fa5cd0161bdf45800277
Bruno Henrique <brunodorea@outlook.com.br>
2024-06-18 14:40:36 -0300

    :sparkles: feat: criando uma lista de itens favoritos

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import * as cartService from "./services/cart.js"
 import createItem from "./services/item.js"
 
 const myCart = []
+const myWishList = []
 
 console.log("Bem vindo ao seu carrinho da Shopee!")
 
 // Criando um item
 const item1 = await createItem("Keyboard", 169.29, 1)
 const item2 = await createItem("Mouse", 69.29, 2)
+const item3 = await createItem("Mouse", 169.29, 1)
 
 await cartService.addItem(myCart, item1)
 await cartService.addItem(myCart, item2)
+await cartService.addItem(myWishList, item3)
 
 await cartService.calculateTotal(myCart)
