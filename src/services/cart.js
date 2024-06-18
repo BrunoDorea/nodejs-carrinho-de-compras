72d6411aad906d2d607484b771de5b9f4c5fe70c
Bruno Henrique <brunodorea@outlook.com.br>
2024-06-18 13:55:10 -0300

    :sparkles: feat: criando as assinaturas do cart.js e do item.js

---
+++ src/services/cart.js
@@ -0,0 +1,12 @@
+// Ações que o carrinho pode fazer
+// Adicionar item no carrinho
+async function addItem(useCart) {}
+
+// Deletar item do carrinho
+async function deleteItem(useCart, name) {}
+
+// Remover item do carrinho
+async function removeItem(useCart, index) {}
+
+// Calcular o total
+async function calculateTotal(useCart) {}
---
+++ src/services/item.js
@@ -0,0 +1,3 @@
+// Ações que o item pode fazer
+// Calcular o sub-total
+async function createItem(name, price, quantity) {}
