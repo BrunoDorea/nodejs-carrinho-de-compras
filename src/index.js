01cc32692e8b7136a55b565767b435278ee7a1cf
Bruno Henrique <brunodorea@outlook.com.br>
2024-06-18 14:05:35 -0300

    :sparkles: feat: implementando a logica do item.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import createItem from "./services/item.js"
+
 const cart = []
+
+console.log("Bem vindo ao seu carrinho da Shopee!")
+
+// Criando um item
+const item1 = await createItem("Keyboard", 169.29, 1)
+const item2 = await createItem("Mouse", 69.29, 2)
+
+console.log(item2.subtotal())
